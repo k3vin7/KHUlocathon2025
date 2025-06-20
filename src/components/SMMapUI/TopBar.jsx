13fed4cb19638deda121aac3c5a74e35fa914aca
kevinc02@naver.com <kevinc02@naver.com>
2025-06-20 12:47:34 +0900

    topbar fixed

--- src/components/SMMapUI/TopBar.jsx
+++ src/components/SMMapUI/TopBar.jsx
@@ -1,6 +1,6 @@
 export default function TopBar({ title }) {
   return (
-    <div className="sticky top-0 left-0 w-full h-[6vh] bg-white bg-opacity-80 z-10 shadow flex items-center justify-between px-4">
+    <div className="fixed top-0 left-0 w-full h-[6vh] bg-white bg-opacity-80 z-10 shadow flex items-center justify-between px-4">
       <h1 className="font-santokki text-[#FF6100] text-[20px]">{title}</h1>
     </div>
   );
