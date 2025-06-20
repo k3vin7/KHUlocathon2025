363e94c9786afffd07d86c10e5a344ff43419598
kevinc02@naver.com <kevinc02@naver.com>
2025-06-20 20:26:34 +0900

    latitude

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -136,7 +136,7 @@ export default function MapContainer({ showMyPage, setShowMyPage, userData, onLo
           walkCircleRef.current = null;
         }
 
-        const offsetLat = place.coordinates.lat - 0.0005;
+        const offsetLat = place.coordinates.lat - 0.00035;
         const targetLatLng = new naver.maps.LatLng(offsetLat, place.coordinates.lng);
         map.morph(targetLatLng, 19, true);
 
