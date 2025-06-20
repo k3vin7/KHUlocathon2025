fb6cdbf8de3bd4179816753a38f2c741a536cd02
scbae18 <scbae18@gmail.com>
2025-06-20 12:33:39 +0900

    카테고리 수정

--- src/components/PCMapUI/MapUIContainer.jsx
+++ src/components/PCMapUI/MapUIContainer.jsx
@@ -6,6 +6,7 @@ export default function MapUIContainer({ isLoggedIn, onLoginClick }) {
   return (
     <div className="absolute top-0 left-0 w-[80px] h-full bg-white z-10 shadow">
         <img src={ logo }/>
+        <POIFilterTabs /> 
         <MenuTabs isLoggedIn={isLoggedIn} onLoginClick={onLoginClick} />
     </div>
   );
--- src/components/SMMapUI/MapUIContainer.jsx
+++ src/components/SMMapUI/MapUIContainer.jsx
@@ -5,7 +5,6 @@ import TopBar from "./TopBar";
 export default function MapUIContainer({ isLoggedIn, onLoginClick, category, setCategory }) {
   return (
     <div>
-      
       <div className="absolute top-[60px] left-0 w-full z-10 p-2">
         <POIFilterTabs category={category} setCategory={setCategory} />
       </div>
